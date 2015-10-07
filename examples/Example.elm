7360fdef268e646f1888f8cbb91bed5d44a1d047
Aaron VonderHaar <gruen0aermel@gmail.com>
2015-10-06 21:57:21 -0700

    Add an edge case to the Table example

--- examples/Example.elm
+++ examples/Example.elm
@@ -3,21 +3,31 @@ import Table
 data =
     [ [ 1, 2, 3, 4 ]
     , [ 10, 5, 24, 3, 7]
+    , []
     , [ 2, 7, 23903, 4039 , 1024, 2 ]
     ]
 
+
+avg : List number -> Maybe Float
 avg items =
-    let
-        sum = List.sum items
-        count = List.length items
-    in
-        (toFloat sum) / (toFloat count)
+    case items of
+        [] ->
+            Nothing
+        _ ->
+            Just <|
+                (toFloat <| List.sum items) / (toFloat <| List.length items)
+
+
+maybeString : Maybe a -> String
+maybeString =
+    Maybe.map toString >> Maybe.withDefault "--"
+
 
 main =
     data
     |> Table.table
         [ ("sum", List.sum >> toString)
-        , ("min", List.foldr min 9999 >> toString)
-        , ("average", avg >> toString)
-        , ("max", List.foldr max -9999 >> toString)
+        , ("min", List.minimum >> maybeString)
+        , ("average", avg >> maybeString)
+        , ("max", List.maximum >> maybeString)
         ]
